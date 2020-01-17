0c1b461eeecba1d3bdb6be5d675251ead1fa3013
Nicolas Bareil <nicolas.bareil@airbus.com>
2020-01-17 16:37:39 +0100

    +

--- _terraform/justanothergeek.tf
+++ _terraform/justanothergeek.tf
@@ -98,7 +98,10 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   viewer_certificate {
     acm_certificate_arn = aws_acm_certificate.https_certificate.arn
     ssl_support_method = "sni-only"
+    minimum_protocol_version = "TLSv1.1_2016"
   }
+
+  wait_for_deployment = false
 }
 
 resource "aws_acm_certificate" "https_certificate" {
