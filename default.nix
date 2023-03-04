b22fe5b1419d6c63a57e47ad40bd7a70128793a1
Nicolas Bareil <nicolas.bareil@airbus.com>
2023-03-04 19:06:37 +0100

    Adding nix

---
+++ default.nix
@@ -0,0 +1,14 @@
+# nixos-21.11 on 2022-01-14 - https://status.nixos.org/
+with (import (fetchTarball https://github.com/nixos/nixpkgs/archive/386234e2a61e1e8acf94dfa3a3d3ca19a6776efb.tar.gz) {});
+
+
+let
+  customPython = pkgs.python38.buildEnv.override {
+  };
+in
+pkgs.mkShell {
+  buildInputs = [
+        pkgs.pre-commit
+        pkgs.hugo
+  ];
+}
